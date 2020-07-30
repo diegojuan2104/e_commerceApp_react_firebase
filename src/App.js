b3f1f4e550f033283f45d9d2629dda0a96e8b80a
diegojuan2104 <diegojuan2104@gmail.com>
2020-07-30 14:55:14 -0500

    implemented firebase utils, authentication with google and email

--- src/App.js
+++ src/App.js
@@ -1,51 +1,64 @@
-import React from 'react';
-import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
 
-import HomePage from './pages/home/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
-import SignInAndSignUp from './pages/sing-in-sign-up/sign-in-sign-up.component'
-import { auth } from './firebase/firebase.utils'
+import HomePage from "./pages/home/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/sing-in-sign-up/sign-in-sign-up.component";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 const HatsPage = () => (
   <div>
-    <h1>
-        HATS PAGE
-    </h1>
+    <h1>HATS PAGE</h1>
   </div>
 );
 
-class  App extends React.Component {
-  constructor(){
+class App extends React.Component {
+  constructor() {
     super();
     this.state = {
-      currentUser: null
-    }
+      currentUser: null,
+    };
   }
 
-  componentDidMount(){
-    auth.onAuthStateChanged(user => {
-      this.setState({currentUser:user});
-      console.log(user);
-    })
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          }, () => {
+            console.log(this.state);
+          });
+        });
+      } else {
+        this.setState({
+          currentUser: userAuth,
+        });
+      }
+    });
   }
 
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
 
-  render(){
+  render() {
     return (
       <div>
         <Header currentUser={this.state.currentUser}></Header>
         <Switch>
-          <Route exact path='/' component={ HomePage }></Route>
-          <Route exact path='/shop' component={ ShopPage }></Route>
-          <Route exact path='/signin' component={ SignInAndSignUp }></Route>
-          <Route exact path='/shop/hats' component={ HatsPage }></Route>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route exact path="/shop" component={ShopPage}></Route>
+          <Route exact path="/signin" component={SignInAndSignUp}></Route>
+          <Route exact path="/shop/hats" component={HatsPage}></Route>
         </Switch>
       </div>
     );
---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,96 @@
+import React from 'react'
+
+import FormInut from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
+
+import {auth, createUserProfileDocument} from '../../firebase/firebase.utils'
+
+import './sign-up.styles.scss'
+import FormInput from '../form-input/form-input.component'
+
+class SignUp extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            displayName : '', 
+            email : '',
+            password : '',
+            confirmPassword : ''
+        }
+    }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+        const { displayName, email, password, confirmPassword} = this.state;
+
+        if(password != confirmPassword) {
+            alert("passwords don't match")
+            return;
+        }
+
+        try{
+            const { user } = await auth.createUserWithEmailAndPassword(email,password)
+            createUserProfileDocument(user, {displayName});
+
+        }catch(error){
+            console.error(error)
+        }
+    }
+
+    handleChange = event =>{
+        const {name, value} = event.target;
+        this.setState({[name]:value})
+    }
+
+    render(){
+        const { displayName, email, password, confirmPassword} = this.state;
+        return(
+            <div className = 'sign-up'>
+                <h2>I don't have an account</h2>
+                <span>Sign UP with your email and password</span>
+                <form className = 'sign-up-form' onSubmit={this.handleSubmit}>
+                    <FormInput 
+                        handleChange={this.handleChange} 
+                        type='text'
+                        name='displayName'
+                        value={displayName}
+                        label='Display Name'
+                        required
+                        >
+                    </FormInput>
+                    <FormInput 
+                        handleChange={this.handleChange} 
+                        type='email'
+                        name='email'
+                        value={email}
+                        label='Email'
+                        required
+                        >
+                    </FormInput>
+                    <FormInput 
+                        handleChange={this.handleChange} 
+                        type='password'
+                        name='password'
+                        value={password}
+                        label='Password'
+                        required
+                        >
+                    </FormInput>
+                    <FormInput 
+                        handleChange={this.handleChange} 
+                        type='password'
+                        name='confirmPassword'
+                        value={confirmPassword}
+                        label='Confirm Password'
+                        required
+                        >
+                    </FormInput>
+
+                    <CustomButton type='submit'>SIGN UP</CustomButton>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SignUp;
