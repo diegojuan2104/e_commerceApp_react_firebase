28d17ba5ad54b4b596f319f8ab9efc2e740486c9
diegojuan2104 <diegojuan2104@gmail.com>
2020-08-09 17:38:21 -0500

    R

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -8,8 +8,8 @@ const CollectionPreview = ({title, items}) => (
     <div className='collection-preview'>
         <h1>{title.toUpperCase()}</h1>
         <div className="preview">
-            {items.filter((item,idx)=> idx < 4).map(({id, ...otherItemProps}) => (
-                <CollectionItem key={id}{... otherItemProps}></CollectionItem>
+            {items.filter((item,idx)=> idx < 4).map((item) => (
+                <CollectionItem key={item.id} item = {item}></CollectionItem>
             ))}
         </div>
     </div>
